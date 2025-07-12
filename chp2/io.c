cf342b4319481a62246db4066cd6cfb0e8eb0458
0xull <mrikehchukwuka@gmail.com>
2025-07-12 03:13:04 +0100

    basic i/o ops

--- chp2/io.c
+++ chp2/io.c
@@ -1,4 +1,13 @@
+#include <fcntl.h>
+#include <assert.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[]) 
 {
-	
+	int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC | S_IRWXU);
+	assert(fd > -1);
+	int rc = write(fd, "Hello, world!\n", 15);
+	assert(rc == 15);
+	close(fd);
+	return 0;
 }
